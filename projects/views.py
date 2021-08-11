e3b80a677b4d5485a4c1dfd90451a9d5e183f420
arogya <arogya247@gmail.com>
2021-08-12 03:13:23 +0530

    if else tags added and context dict is  added

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'the projects'
+    number = 12
+    context = {'page':page, 'number':number}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
