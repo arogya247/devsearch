45b560d37cc54a0ae8122dac4cec8c292a96c896
arogya <arogya247@gmail.com>
2021-08-14 02:44:52 +0530

    styled user-profile template

--- users/views.py
+++ users/views.py
@@ -9,5 +9,10 @@ def profiles(request):
 
 
 def userProfile(request, pk):
-    
-    return render(request, 'users/user-profile.html')
+    profile = Profile.objects.get(id=pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile':profile, 'topSkills':topSkills, 'otherSkills':otherSkills}
+    return render(request, 'users/user-profile.html', context)
